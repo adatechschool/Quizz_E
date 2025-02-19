a440722948c394c4fbd1d38f95e919765f2b3d40
Rerefenec <renaud.delorme@free.fr>
2025-02-19 11:30:55 +0100

    carte de france

---
+++ game.js
@@ -0,0 +1,30 @@
+export const quiz_fatoumata_kebe =
+
+
+
+
+ {
+    questions: [
+      {
+        text: "question 0",
+        options: [
+          "L'astronomie, spécialisée dans les débris spatiaux.",
+          "La création de lunettes de réalité augmentée pour observer les étoiles.",
+          "L'étude de la météo sur Mars et ses implications pour l'agriculture spatiale.",
+          "La cartographie des astéroïdes qui pourraient potentiellement entrer en collision avec la Terre."
+        ],
+        correct_answer: "L'astronomie, spécialisée dans les débris spatiaux."
+      },
+      {
+        text: "question 1  Kébé est-elle impliquée ?",
+        options: [
+          "Elle enseigne l'astrophotographie aux astronautes de la Station spatiale internationale.",
+          "Elle milite pour l'éducation des jeunes filles en astronomie.",
+          "Elle entraîne des robots pour des compétitions de quiz interstellaires.",
+          "Elle développe des applications pour cultiver des potagers en apesanteur."
+        ],
+        correct_answer: "Elle milite pour l'éducation des jeunes filles en astronomie."
+      },
+   
+    ]
+  };
---
+++ script.js
@@ -0,0 +1,4 @@
+import { quiz_fatoumata_kebe } from './game.js'
+let index = 1
+const question1 = document.querySelector('.question')
+question1.innerText = quiz_fatoumata_kebe.questions[index].text
